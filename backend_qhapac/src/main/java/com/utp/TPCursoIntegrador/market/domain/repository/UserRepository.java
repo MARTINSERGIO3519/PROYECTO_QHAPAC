2b76debbc022653eb1b680f715ce32e06baf5a9e
Luxorift <szanibal@hotmail.com>
2025-10-26 21:44:53 -0500

    Funcionamiento del apartado de Administrador

--- backend_qhapac/src/main/java/com/utp/TPCursoIntegrador/market/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utp.TPCursoIntegrador.market.domain.repository;
-
-import com.utp.TPCursoIntegrador.market.persistance.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    Optional<Users> findByCorreo(String correo);
-}
